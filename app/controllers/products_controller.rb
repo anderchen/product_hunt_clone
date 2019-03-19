44b51644f55163494dbf854fbc8eca5b424eda8d
Anderson Chen <ander.chen00@gmail.com>
2019-03-19 10:09:55 -0300

    Made some minor corrections

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
-  skip_before_action :authentication_user!, only: :index
+  skip_before_action :authenticate_user!, only: :index
 
   def index
-    @product = Product.all
+    @products = Product.all
   end
 end
